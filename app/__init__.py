59df59d63e73d33543e5bb83a33cf1438ea10863
Jack Heal <jack.heal@bristol.ac.uk>
2016-08-03 15:59:54 +0100

    uses instance config files.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+app.config.from_pyfile('config.py')
 
 from app import views, models
