e24a7905d7374b63fc2e4dba49ef47b5af7c56f1
Jack Heal <jack.heal@bristol.ac.uk>
2016-08-03 16:26:30 +0100

    Refactored base setup to get rid of flask tutorial files.

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+DEBUG = False
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
 TEMP_FOLDER = os.path.join(basedir, 'tmp')
