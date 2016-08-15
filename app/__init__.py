87a8e4e0666938f12ee80fa6d0a1380c5fc1fd8d
Jack Heal <jack.heal@bristol.ac.uk>
2016-08-15 11:41:09 +0100

    Added atlas.db and corresponding database migrations folder.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app=app, db=db)
 
 from app import views, models
 from .util import assets
