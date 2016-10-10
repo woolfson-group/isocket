ffa8d4592c3eefc61d90121282a98cf0716236d2
Jack Heal <jack.heal@bristol.ac.uk>
2016-10-10 17:52:12 +0100

    Added event listener to ensure foreign key constraints are always on (ensure cascade deletions are run).

--- isocket_app/__init__.py
+++ isocket_app/__init__.py
@@ -2,6 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
@@ -9,5 +18,5 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app=app, db=db)
 
-from isocket_app import views, models
+from isocket_app import views, models, populate_models
 from .util import assets
