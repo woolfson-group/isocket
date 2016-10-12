d7364915af9eaa266adb1376d0394230bc70936e
Jack Heal <jack.heal@bristol.ac.uk>
2016-10-12 11:38:15 +0100

    Added new files for app factory

---
+++ isocket_app/factory.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .extensions import db
+
+
+def create_app(config_filename=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config')
+    app.config.from_pyfile('config.py')
+    if config_filename is not None:
+        app.config.from_pyfile(config_filename)
+    db.init_app(app)
+    return app
+
