81fbf996a1c617a7bcfb078ea8e2a2cfef5538a8
Jack Heal <jack.heal@bristol.ac.uk>
2016-10-13 16:06:22 +0100

    Added static_url_path for blueprint static files.

--- isocket_app/home/__init__.py
+++ isocket_app/home/__init__.py
@@ -4,7 +4,8 @@ home = Blueprint(
     'home',
     __name__,
     template_folder='templates',
-    static_folder='static'
+    static_folder='static',
+    static_url_path='/{}'.format(__name__)
 )
 
 from isocket_app.home import views
