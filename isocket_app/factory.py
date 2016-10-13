63d22b8949043ee74787e36ab607655b7bf480b1
Jack Heal <jack.heal@bristol.ac.uk>
2016-10-13 19:11:21 +0100

    Moved new create_app function to factory.py and call this in run.py

--- isocket_app/factory.py
+++ isocket_app/factory.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from .extensions import db
+from flask_uploads import UploadSet, configure_uploads
+from flask_migrate import Migrate
 
 
 def create_app(config_filename=None):
@@ -8,6 +9,22 @@ def create_app(config_filename=None):
     app.config.from_pyfile('config.py')
     if config_filename is not None:
         app.config.from_pyfile(config_filename)
+    # Database set up
+    from isocket_app.models import db
     db.init_app(app)
+    # Register Blueprints
+    from isocket_app.home import home_bp
+    app.register_blueprint(home_bp)
+    from isocket_app.atlas import atlas_bp
+    app.register_blueprint(atlas_bp)
+    from isocket_app.structure import structure_bp
+    app.register_blueprint(structure_bp)
+    # Flask-assets, flask-uploads, flask-migrate.
+    from isocket_app.util.assets import bundles, assets
+    assets.init_app(app)
+    assets.register(bundles)
+    migrate = Migrate(app=app, db=db)
+    structures = UploadSet(name='structures', extensions=app.config['UPLOADED_STRUCTURES_ALLOW'])
+    configure_uploads(app, structures)
     return app
 
