87ed41d0dbb4967db3f75ada8b088caada1824f9
Jack Heal <jack.heal@bristol.ac.uk>
2016-10-14 11:23:39 +0100

    Moved configuration settings into object structure. Have environmental variable ISOCKET_CONFIG that can be set to "development" or "testing".

--- config.py
+++ config.py
@@ -1,12 +1,46 @@
 import os
-
-WTF_CSRF_ENABLED = True
-DEBUG = False
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-STATIC_FOLDER = os.path.join(basedir, 'isocket_app', 'static')
-UPLOADS_DEFAULT_DEST = os.path.join(STATIC_FOLDER, 'uploads')
-UPLOADED_STRUCTURES_DEST = os.path.join(UPLOADS_DEFAULT_DEST, 'structures')
-TEMP_FOLDER = os.path.join(basedir, 'tmp')
-UPLOADED_STRUCTURES_ALLOW = ('pdb', 'mmol', 'cif', 'mmcif')
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = 'change_this_key'
+    # sqlite :memory: identifier is the default if no filepath is present
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    # Uploads folders for flask-uploads
+    STATIC_FOLDER = os.path.join(basedir, 'isocket_app', 'static')
+    UPLOADS_DEFAULT_DEST = os.path.join(STATIC_FOLDER, 'uploads')
+    UPLOADED_STRUCTURES_DEST = os.path.join(UPLOADS_DEFAULT_DEST, 'structures')
+    TEMP_FOLDER = os.path.join(basedir, 'tmp')
+    UPLOADED_STRUCTURES_ALLOW = ('pdb', 'mmol', 'cif', 'mmcif')
+    ASSETS_DEBUG = DEBUG
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.path.join('sqlite://', basedir, 'isocket_app', 'atlas.db')
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.path.join('sqlite://', basedir, 'unit_tests', 'test_atlas.db')
+
+
+config = {
+    "development": "isocket.config.DevelopmentConfig",
+    "testing": "isocket.config.TestingConfig",
+    "default": "isocket.config.DevelopmentConfig"
+}
+
+
+def configure_app(app):
+    # get env variable ISOCKET_CONFIG, if doesn't exist, set to 'default'
+    config_name = os.getenv(key='ISOCKET_CONFIG', default='default')
+    app.config.from_object(config[config_name])
+    app.config.from_pyfile('config.cfg', silent=True)
+
+
