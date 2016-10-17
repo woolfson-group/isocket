792c5afa6f81be076ebcd3b54caf9dea36394614
Jack Heal <jack.heal@bristol.ac.uk>
2016-10-17 12:44:45 +0100

    Added setUp function to PdbDBTestCase

--- unit_tests/test_database.py
+++ unit_tests/test_database.py
@@ -3,10 +3,11 @@ from flask_testing import TestCase
 
 from isocket_app.factory import create_app
 from isocket_app.extensions import db
-from isocket_app.populate_models import add_pdb_code, populate_cutoff
+from isocket_app.populate_models import add_pdb_code, remove_pdb_code, populate_cutoff
 from isocket_app.models import PdbDB, CutoffDB
 
 os.environ['ISOCKET_CONFIG'] = 'testing'
+
 
 class BaseTestCase(TestCase):
 
@@ -24,12 +25,24 @@ class BaseTestCase(TestCase):
 
 class PdbDBTestCase(BaseTestCase):
 
+    def setUp(self):
+        super().setUp()
+        self.test_code = '2ebo'
+        self.query_pdb = db.session.query(PdbDB).filter(PdbDB.pdb == self.test_code)
+
     def test_add_pdb_code(self):
-        code = '2ebo'
+        code = self.test_code
         add_pdb_code(code, session=db.session)
         q = db.session.query(PdbDB).filter(PdbDB.pdb == code)
         p = q.one()
         self.assertEqual(p.pdb, code)
+
+    """
+    def test_remove_pdb_code(self):
+        code = self.test_code
+        remove_pdb_code(code, session=db.session)
+        q = db.session.query(PdbDB).filter(PdbDB.pdb == code)
+    """
 
 
 class FixedTablesTestCase(BaseTestCase):
