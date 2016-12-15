0a186f5d0808ef2182fdf6990dad592b6bb69695
Jack Heal <jack.heal@bristol.ac.uk>
2016-12-15 15:22:28 +0000

    Removed tests for unused function sorted_connected_components

--- unit_tests/test_graph_theory.py
+++ unit_tests/test_graph_theory.py
@@ -5,7 +5,7 @@ import numpy
 from networkx.generators import cycle_graph, complete_graph
 
 from isocket_settings import global_settings
-from isocket.graph_theory import AtlasHandler, isomorphism_checker, sorted_connected_components
+from isocket.graph_theory import AtlasHandler, isomorphism_checker
 
 mode = 'testing'
 unknown_graphs = global_settings["unknown_graphs"][mode]
@@ -52,45 +52,5 @@ class IsomorphismCheckerTestCase(unittest.TestCase):
     def test_complete_graph(self):
         g = complete_graph(8)
         self.assertIsNone(isomorphism_checker(g))
-
-
-class SortedConnectedComponentsTestCase(unittest.TestCase):
-    """Tests for isambard.tools.graph_theory.sorted_connected_components. """
-
-    def test_number_of_disjoint_cycles(self):
-        """Graph of k disjoint cycles should have k connected components."""
-        n = numpy.random.randint(4, 100)
-        cycles = [networkx.cycle_graph(x) for x in range(3, n)]
-        g = networkx.disjoint_union_all(cycles)
-        self.assertEqual(len(sorted_connected_components(g)), len(cycles))
-
-    def test_number_of_trivial_nodes(self):
-        """Graph with no edges should have 0 connected components."""
-        n = numpy.random.randint(1, 100)
-        g = networkx.Graph()
-        g.add_nodes_from(list(range(n)))
-        self.assertEqual(len(sorted_connected_components(g, include_trivials=False)), 0)
-        self.assertEqual(len(sorted_connected_components(g, include_trivials=True)), n)
-
-    def test_cc_cyclic(self):
-        """Largest connected component of a cyclic graph should be itself."""
-        n = numpy.random.randint(3, 101)
-        cycle = cycle_graph(n)
-        cc0 = sorted_connected_components(cycle)[0]
-        aet = networkx.is_isomorphic(cycle, cc0)
-        self.assertTrue(aet)
-
-    def test_cc_two_cycles(self):
-        g = networkx.disjoint_union(cycle_graph(4), cycle_graph(3))
-        components = sorted_connected_components(g)
-        cc_nodes = [x.number_of_nodes() for x in components]
-        self.assertTrue(numpy.allclose(cc_nodes, [4, 3]))
-
-    def test_non_trivial(self):
-        """Add trivial nodes: these should be removed by call to get_connected_component."""
-        g = networkx.cycle_graph(5)
-        g.add_nodes_from([5, 6, 7, 8])
-        components = sorted_connected_components(g, include_trivials=False)
-        self.assertEqual(components[0].nodes(), list(range(5)))
 
 __author__ = 'Jack W. Heal'
