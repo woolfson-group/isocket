5b663394da380957246206e2f8e6a90ecdcc425e
jackheal <jack.heal@cruk.manchester.ac.uk>
2017-06-16 16:20:33 +0100

    Removed app_path so that this now works with bokeh 0.12.6

--- isocket/atlas/views.py
+++ isocket/atlas/views.py
@@ -5,6 +5,6 @@ from isocket.atlas import atlas_bp
 
 @atlas_bp.route('/atlas')
 def atlas():
-    script = autoload_server(model=None, app_path='/atlas')
+    script = autoload_server(model=None, url='http://localhost:5006/atlas')
     return render_template('atlas.html', title='AtlasCC', bokeh_script=script)
 
