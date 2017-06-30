f73a9c501914d888190b44ae3e89ce98b156f130
jackheal <jack.heal@cruk.manchester.ac.uk>
2017-06-30 11:45:47 +0100

    Added Dockerfiles and docker-compose.

--- web/main.py
+++ web/main.py
@@ -5,4 +5,4 @@ from web.isocket.factory import create_app
 os.environ['ISOCKET_CONFIG'] = 'development'
 app = create_app()
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
