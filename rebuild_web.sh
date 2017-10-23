d36b2a681bc3927aecd7dd3f8a1f1d1259810c2a
Chris Wells Wood <cwwoodesq@gmail.com>
2017-10-23 14:05:07 +0100

    Modified rebuild script to remove sudo.

--- rebuild_web.sh
+++ rebuild_web.sh
@@ -1,8 +1,8 @@
-sudo docker stop isocket_web_1
-sudo docker stop isocket_atlas_1
-sudo docker rm isocket_web_1
-sudo docker rm isocket_atlas_1
-sudo docker build -t isocket_web ./web/
-sudo docker build -t isocket_atlas ./atlas_visualisation/
-sudo docker run -d -p 1803:5006 --name isocket_atlas_1 isocket_atlas
-sudo docker run -d -p 1802:80 --restart on-failure --name isocket_web_1 isocket_web
+docker stop isocket_web_1
+docker stop isocket_atlas_1
+docker rm isocket_web_1
+docker rm isocket_atlas_1
+docker build -t isocket_web ./web/
+docker build -t isocket_atlas ./atlas_visualisation/
+docker run -d -p 1803:5006 --name isocket_atlas_1 isocket_atlas
+docker run -d -p 1802:80 --restart on-failure --name isocket_web_1 isocket_web
